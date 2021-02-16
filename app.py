600db60338094e54e7d6a2a36627f92f8e68b455
Johannes Holst <johannes.holst1@gmail.com>
2021-02-16 15:34:02 +0100

    Uploaded files and stuff

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route("/memes")
+def memes():
+    return render_template('memes.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/HistoryMemes")
+def HistoryMemes():
+    return render_template('HistoryMemes.html')
+
+@app.route("/WorldWar")
+def WorldWar():
+    return render_template('WorldWar.html')
+
+@app.route("/Communist")
+def Communist():
+    return render_template('Communist.html')
+
+@app.errorhandler(404)
+def handler404(_):
+    return render_template('404.html')
+
+
+if __name__ == "__main__":
+    app.run()
